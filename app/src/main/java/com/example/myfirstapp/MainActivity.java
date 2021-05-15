aa2701b4e919bff059ff7f230d5193cb159144f8
Melsen <melsen@yandex.ru>
2021-05-15 23:39:18 +0300

    Выполнил пункт 4

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -11,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
         //setContentView(R.layout.activity_two);
+        //Атрибут EMS используется для определения значения ширины TextView
+        // в единицах измерения EM. При этом EM это единица измерения в Typography (Типография),
+        // его значением является ширина буквы "M"
         setContentView(R.layout.activity_three);
 
     }
